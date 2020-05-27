8d277f9d6236a2dff0f206977484e66ef227b889
4lexRossi <financeiro.alexsander@gmail.com>
2020-05-27 11:51:15 -0300

    atualização experiencias e certificados

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,6 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
-  // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -14,15 +13,13 @@
       }
     }
   });
-
-  // Closes responsive menu when a scroll trigger link is clicked
+  
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#sideNav'
   });
 
-})(jQuery); // End of use strict
+})(jQuery);
